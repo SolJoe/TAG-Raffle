8e7d92b0f3730821daa8edca40acda2d021fd1f8
joe <solanaghosts@gmail.com>
2022-04-10 09:03:39 -0400

    added new raffle

--- src/config/raffleWhitelist.ts
+++ src/config/raffleWhitelist.ts
@@ -2,85 +2,31 @@ import { PublicKey } from '@solana/web3.js';
 import { RaffleMetaData } from '../lib/types';
 import { TESTING } from './misc';
 
-const testWhitelist = new Map<string, RaffleMetaData>([
-  ['3gce7612sT3pTWtAYp5xcU2fjEfEX1U7tEFHyoftbKD5', { name: 'dRafflenet #1' }],
-  [
-    '3u7t4uGkMP1VW5obT78rgk74zdATTuNuy6BWPfRucLfj',
-    {
-      name: 'dRaffle for the win',
-      alternatePurchaseMints: [
-        new PublicKey('72UgZ7avdJZBbv3wR7hbWcFy6dyHHNAoJw7CimGA55Zh'),
-      ],
-    },
-  ],
-  [
-    '2mrwjEz67DXTWKaVPjWwkwVPtGRWyKrTeyK5VzintsC5',
-    { name: 'dRaffle with a loooooooooooooooong name' },
-  ],
-  [
-    '9FoUjfUpWwhHYaGKM9G5eYab7qow3oWqdo2G5Ehj3h5L',
-    {
-      name: 'Oh my dRaffle',
-      overviewImageUri: '/resources/001-mainnet-launch.gif',
-      alternatePurchaseMints: [
-        new PublicKey('So11111111111111111111111111111111111111112'),
-      ],
-    },
-  ],
-]);
+// const testWhitelist = new Map<string, RaffleMetaData>([
+// ]);
 
 const prodWhitelist = new Map<string, RaffleMetaData>([
-  [
+  [ 
     '3gce7612sT3pTWtAYp5xcU2fjEfEX1U7tEFHyoftbKD5',
     {
       name: 'DADBOD NFT',
       // overviewImageUri: '/resources/001-mainnet-launch.gif',
     },
   ],
-  [
-    '2QjkshNu3mrcCnriekTpppa3PFwnAR9Yf7v5vc54m2Yh',
+  [ 
+    'D4iojpC3Dz7iLaGb4fxRCkLdgftBLbgMStoX9qoCucLE',
     {
-      name: 'First SOL raffle',
-      overviewImageUri: '/resources/solana-logo.gif',
+      name: 'NFT Raffle',
+      // overviewImageUri: '/resources/001-mainnet-launch.gif',
     },
   ],
-  [
-    '8aEm1MoDqkYT5vCB21jC6aMMcMbdQJgmHpyBbtHDfUjU',
+  [ 
+    '9aDZyjaw7yJ5UEUEsT7iSg9KMkd6ixnbrzM4Ya38BagL',
     {
-      name: 'Anti Artist Club',
-      overviewImageUri: '/resources/aartist-raffle-overview.gif',
+      name: 'NFT Raffle Test',
+      // overviewImageUri: '/resources/001-mainnet-launch.gif',
     },
-  ],
-  [
-    '2ziwAj4awgvNyn8ywwjkBRkBsmv259u9vVyEdrGYTb54',
-    {
-      name: 'More SOL',
-      overviewImageUri: '/resources/solana-logo.gif',
-    },
-  ],
-  [
-    'EgHys3WPcM5WRpKqVHs1REfK6Npzq9sJ7dZPFPzQy2xG',
-    {
-      name: 'Triple SOL',
-      overviewImageUri: '/resources/solana-logo-x3.gif',
-    },
-  ],
-  [
-    'CjzFZfrMW4D1jZVm5upCobRi96UYnQTk5cescSt12rhV',
-    {
-      name: 'SAMO raffle',
-      overviewImageUri: '/resources/samo-x3.gif',
-    },
-  ],
-  [
-    'EZtBKgWq66KT4jRKtd4VT3LWh3mVC4pwcCsqLzKas63G',
-    {
-      name: 'BitBoat raffle',
-      overviewImageUri: '/resources/bitboat-raffle.gif',
-    },
-  ],
+  ] 
 ]);
 
-export const RAFFLES_WHITELIST = TESTING
-  ? testWhitelist
-  : prodWhitelist;
+export const RAFFLES_WHITELIST = prodWhitelist;
