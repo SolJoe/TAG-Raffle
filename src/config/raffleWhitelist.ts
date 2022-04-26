50a0334a1a12e82c4aa87f89488d3388d34deddd
SolJoe <solanaghosts@gmail.com>
2022-04-26 12:26:22 -0400

    adding new raffle

--- src/config/raffleWhitelist.ts
+++ src/config/raffleWhitelist.ts
@@ -7,58 +7,16 @@ import { TESTING } from './misc';
 
 const prodWhitelist = new Map<string, RaffleMetaData>([
   [ 
+    '41ujFNJ5QCGdkgVtGtYMne3MSv19iLR9cyHko7YJ7pJK',
+    {
+      name: 'Miners of Mars #2521',
+      // overviewImageUri: '/resources/001-mainnet-launch.gif',
+    },
+  ],
+  [ 
     'H6aDpAbkfNZZZ3TkPhe8crg64MMnMh2DzxRDWcds4reb',
     {
       name: 'Solana Hills #1465',
-      // overviewImageUri: '/resources/001-mainnet-launch.gif',
-    },
-  ],
-  [ 
-    '5oQqs4nheYLLYt5tv5DnmezN768fRXjFWKCWbjxkmwmB',
-    {
-      name: 'Serum Surfers #2956',
-      // overviewImageUri: '/resources/001-mainnet-launch.gif',
-    },
-  ],
-  [ 
-    'AeNC8DncCzrzw8xGg31kHGFJoq1W5ZykRE3SqsKm1p4o',
-    {
-      name: 'Saigo Monkeys #842',
-      // overviewImageUri: '/resources/001-mainnet-launch.gif',
-    },
-  ],
-  [ 
-    '5a4VngLuzP6AV6C1ndvEcrAw2GgwZQdxdPFi8McqjjV5',
-    {
-      name: 'Solminator 237',
-      // overviewImageUri: '/resources/001-mainnet-launch.gif',
-    },
-  ],
-  [ 
-    'HAWBikaGUWw7trXan6MJCD2yxheygLAopC3nW8dHVNr9',
-    {
-      name: 'Soul of Hidama #232',
-      // overviewImageUri: '/resources/001-mainnet-launch.gif',
-    },
-  ],
-  [ 
-    '86JqdpnZwn9qP8Qft44aD5a2onDAzDoLeM4frhjTdFAi',
-    {
-      name: 'Cheetah Coalition #1534',
-      // overviewImageUri: '/resources/001-mainnet-launch.gif',
-    },
-  ],
-  [ 
-    'Dmectrd4SXZq7BnyBzB22qxiFvVUb6VgDyDAvKbBeztc',
-    {
-      name: 'Flavor Flamez #3140',
-      // overviewImageUri: '/resources/001-mainnet-launch.gif',
-    },
-  ],
-  [ 
-    'HXyu5ANjJ3L7oz21aZcjkKcm6echY17ejSBCVJfj1KV5',
-    {
-      name: 'Parcl HOA #1512',
       // overviewImageUri: '/resources/001-mainnet-launch.gif',
     },
   ]
