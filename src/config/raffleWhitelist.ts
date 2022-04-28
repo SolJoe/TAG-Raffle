63d14f201d2dadd267dc59b7af8eb4a3f1385b5f
SolJoe <solanaghosts@gmail.com>
2022-04-27 22:17:51 -0400

    adding new raffle

--- src/config/raffleWhitelist.ts
+++ src/config/raffleWhitelist.ts
@@ -26,6 +26,13 @@ const prodWhitelist = new Map<string, RaffleMetaData>([
       name: 'Solana Hills #1465',
       // overviewImageUri: '/resources/001-mainnet-launch.gif',
     },
+  ],
+  [ 
+    '5oQqs4nheYLLYt5tv5DnmezN768fRXjFWKCWbjxkmwmB',
+    {
+      name: 'Serum Surfers #2956',
+      // overviewImageUri: '/resources/001-mainnet-launch.gif',
+    },
   ]
    
 ]);
