c80e0a230bdcd4af50324056934a0afbe3672e9f
SolJoe <solanaghosts@gmail.com>
2022-05-08 00:54:52 -0400

    adding new raffle

--- src/config/raffleWhitelist.ts
+++ src/config/raffleWhitelist.ts
@@ -6,45 +6,11 @@ import { TESTING } from './misc';
 // ]);
 
 const prodWhitelist = new Map<string, RaffleMetaData>([
-  [ 
-    '2921MEPA2HJp6jXa7tvJAzt3bibcuDP8cdXZmAgFLDB4',
-    {
-      name: 'Wise Cats #1986',
-      // overviewImageUri: '/resources/001-mainnet-launch.gif',
-    },
-  ],
-  [ 
-    '4EzCUnJvqxwEvmgbLj9b2KeWxZmNRupug8vtV3y8RoTD',
-    {
-      name: 'Dead Reject #3403',
-      // overviewImageUri: '/resources/001-mainnet-launch.gif',
-    },
-  ],
-  [ 
-    '24GzbqMuBv4FMecWHrbP8imNS3mh6deftNazd9HUMqcG',
-    {
-      name: 'Lavish Lions #2422',
-      // overviewImageUri: '/resources/001-mainnet-launch.gif',
-    },
-  ],
-  [ 
-    'qUosZbko98T7jkd1XuG66uyhNjHHTUxVu6hq74V1tdM',
-    {
-      name: 'SMU #2199',
-      // overviewImageUri: '/resources/001-mainnet-launch.gif',
-    },
-  ],
+  
   [ 
     '41ujFNJ5QCGdkgVtGtYMne3MSv19iLR9cyHko7YJ7pJK',
     {
       name: 'Miners of Mars #2521',
-      // overviewImageUri: '/resources/001-mainnet-launch.gif',
-    },
-  ],
-  [ 
-    'H6aDpAbkfNZZZ3TkPhe8crg64MMnMh2DzxRDWcds4reb',
-    {
-      name: 'Solana Hills #1465',
       // overviewImageUri: '/resources/001-mainnet-launch.gif',
     },
   ]
