020d110f58346ec96e50bd4fb7d432061bf9d1d4
SolJoe <solanaghosts@gmail.com>
2022-05-10 16:09:16 -0400

    adding new raffle

--- src/config/raffleWhitelist.ts
+++ src/config/raffleWhitelist.ts
@@ -7,6 +7,13 @@ import { TESTING } from './misc';
 
 const prodWhitelist = new Map<string, RaffleMetaData>([
   [ 
+    '8eSwb3SQdqvEXBdqHZQpDhabXi4j1sK7JBaG43Cymprt',
+    {
+      name: 'Raffle Booster GiveAway',
+      // overviewImageUri: '/resources/001-mainnet-launch.gif',
+    },
+  ],
+  [ 
     '7nqfBF61QrS9gk36meXJkgZ4pXA6b2HqupitF2aRtz8M',
     {
       name: 'Yoked Yetis 3 WL Spots',
